ebed7ef810ea2bd0dfa102f0d63e6e2bc1ac8008
Atkins Munodei <amunodei@gamil.com>
2025-09-16 19:33:26 +0200

    my car

---
+++ Pythondatatypes/my_classes.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self, make: str, model:str, year: int):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        return f"{self.year} {self.make} {self.model}"
